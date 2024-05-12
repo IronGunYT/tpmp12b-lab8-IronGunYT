b25492f30f02674942dc6836b4998f79c40e9f1c
IronGun <strannik.kraya575721@gmail.com>
2024-05-12 16:17:57 +0300

    add task 2.1

---
+++ tpmp_lab8_2.1/tpmp_lab8_2.1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  tpmp_lab8_2.1
+//
+//  Created by Demid on 12.05.24.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    NSString * appDelegateClassName;
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+    }
+    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}
